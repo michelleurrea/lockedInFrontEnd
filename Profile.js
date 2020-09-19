cfffe7a9aa99a9a7cb90b560ba2cf097462a47a7
Michelle Urrea <michelleurrea@Michelles-MacBook-Pro.local>
2020-09-19 10:23:47 -0700

    Set up twopages in the navigation bar

---
+++ Profile.js
@@ -0,0 +1,12 @@
+import React, { Componenet } from 'react'
+import { View, Text } from 'react-native';
+
+export default class Profile extends Component {
+    render() {
+        return (
+            <View>
+                <Text>This is the profile page</Text>
+            </View>
+        )
+    }
+}
