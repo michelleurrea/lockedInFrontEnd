85d401a189000ca3b8dc14d3dcf5308e72be5e9a
Michelle Urrea <michelleurrea@Michelles-MacBook-Pro.local>
2020-09-20 14:10:10 -0700

    added in a button to navigate to details page

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Welcome to Locked-In!</Text>
+      <Button
+        titile="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      /> 
     </View>
   );
 }
