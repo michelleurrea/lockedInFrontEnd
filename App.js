b35ca9b98355fb6a4e7457ef7cebf56121e5f226
Michelle Urrea <michelleurrea@Michelles-MacBook-Pro.local>
2020-09-20 14:35:25 -0700

    Used navigation.push instead of navigation.navigate in line 14

--- App.js
+++ App.js
@@ -10,17 +10,21 @@ function HomeScreen({ navigation }) {
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Welcome to Locked-In!</Text>
       <Button
-        titile="Go to Details"
+        title="Go to Details"
         onPress={() => navigation.navigate('Details')}
       /> 
     </View>
   );
 }
 
-function DetailsScreen() {
+function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to Details...again"
+        onPress={() => navigation.push('Details')}
+      />
     </View>
   );
 }
